4b98312a94d2a3aded103b77a4a762004d599dc8
junya-zzz <bp17028@shibaura-it.ac.jp>
2019-12-11 17:09:48 +0900

    2

--- Boundary.java
+++ Boundary.java
@@ -1,9 +1,40 @@
 package receptionSystem;
 
+import java.io.*;
+
 public class Boundary {
+    
+   
 
-	public void inputReqInfo() {
+    public RequestInformation inputReqInfo() {
+	String str1 = null;
+	String str2 = null;
+	String str3 = null;
+	String str4 = null;
+	BufferedReader br = new BufferedReader(new InputStreamReader
+					       (System.in));
 
+	try{
+	    System.out.print("clientName => ");
+	    str1 = br.readLine();
+	    System.out.print("clentPhoneNum => ");
+	    str2 = br.readLine();
+	    System.out.print("receiverName => ");
+	    str3 = br.readLine();	    
+	    System.out.print("receiverAddress => ");
+	    str4 = br.readLine();
+
+	    return new RequestInformation(str1,str2,str3,str4);
+
+
+
+	}catch(IOException e){
+	    e.printStackTree();
 	}
-
+    }
+        
 }
+
+
+
+
--- Reception.java
+++ Reception.java
@@ -4,31 +4,38 @@ import recordSystem.LuggageList;
 
 public class Reception {
 
-	private LuggageList lugList;
+    private LuggageList lugList;
 
-	private Boundary boundary;
+    private Boundary boundary;
 
-	private Signal signal;
+    private Signal signal;
 
-	public void sendDeliveryRecord() {
 
-	}
+    public Reception(){
+	this.ligList = new LuggageList();
+	this.boundary = new Boundary();
+	this.signal = new Signal();
+    }
+    /*配達記録を本部に送る*/
+    public void sendDeliveryRecord() {
 
-	/*発送時間を本部に報告する*/
-	public void sendShipTime() {
+    }
 
-	}
+    /*発送時間を本部に報告する*/
+    public void sendShipTime() {
 
-	public void getIsDelivery() {
+    }
+    /*中継所との引渡し結果を得る*/
+    public void getIsDelivery() {
 
-	}
+    }
+    /*荷物を収集担当ロボットに渡す*/
+    public void sendLug() {
 
-	public void sendLug() {
+    }
+    /*荷物を依頼人から受け取る*/
+    public void getLug() {
 
-	}
-
-	public void getLug() {
-
-	}
+    }
 
 }
