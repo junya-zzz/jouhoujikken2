17686035fc31e2891e1bb303f28bee759ee9e3c6
bp17048 <bp17048@shibaura-it.ac.jp>
2019-12-12 20:49:31 +0900

    add javadoc

--- CollectingRobot.java
+++ CollectingRobot.java
@@ -19,10 +19,17 @@ public class CollectingRobot extends Robot {
 		closeSensors();
 	}
 
+	/**
+	 * 配達の成功/失敗を切り替える
+	 * @param result 配達の成否
+	 */
 	private void changeIsDelivery(boolean result) {
 		isDelivery = result;
 	}
 
+	/**
+	 * 荷物を中継所に引き渡す
+	 */
 	public void sendLug() {
 		signal = new EV3Signal();
 		boolean connectionStat = signal.openSig(relaySystem, 10000);
@@ -41,10 +48,16 @@ public class CollectingRobot extends Robot {
 		return;
 	}
 
+	/**
+	 * 中継所から宅配受付所に移動する
+	 */
 	private void relayStationToReception() {
 
 	}
 
+	/**
+	 * 配達が成功したか失敗したかを宅配受付所に送信する
+	 */
 	public void sendIsDelivery() {
 		signal = new EV3Signal();
 		try {
@@ -59,10 +72,16 @@ public class CollectingRobot extends Robot {
 		}
 	}
 
+	/**
+	 * 宅配受付所から中継所に移動する
+	 */
 	private void receptionToRelayStation() {
 
 	}
 
+	/**
+	 * 宅配受付所から荷物を受け取る
+	 */
 	public void getLugfrom() {
 		signal = new EV3Signal();
 		try {
