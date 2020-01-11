8ea1de557beb3281f7fcf41d1a9c4ce6f3be5107
keita-kaneko <58538363+keita-kaneko@users.noreply.github.com>
2020-01-11 14:07:36 +0900

    Add files via upload

--- relaySystem/RelayStation.java
+++ relaySystem/RelayStation.java
@@ -2,6 +2,9 @@ package relaySystem;
 
 import java.util.ArrayList;
 import java.util.Date;
+
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
 import recordSystem.*;
 import signal.*;
 
@@ -34,12 +37,15 @@ public class RelayStation {
 	 * 手順
 	 * メソッドに対応する操作を行う
 	 */
-	public void receiveLugfromGatheringRobot() {
+	public void receiveLugfromCollectingRobot() {
 		try{
-			signal.openSig("GatheringRobot");
 			signal.waitSig();
 			Luggage luggage = (Luggage)signal.getSig();
 			luggageList.add(luggage);
+			LCD.clear();
+			LCD.drawString("luggage:"+luggageList.get(0).getLuggageID(), 0, 2);
+			Delay.msDelay(5000);
+			LCD.refresh();
 			signal.sendSig("Receiving completed.");
 			signal.closeSig();
 		}catch(Exception e){
@@ -53,11 +59,15 @@ public class RelayStation {
 	 * 手順
 	 * メソッドに対応する操作を行う
 	 */
-	public void reportReceivingToHeadquarters() {
+	public void reportReceivingToHeadquarters(Date ship_time) {
 		try{
-			signal.openSig("Headquarters");
-			signal.waitSig();
+			signal.openSig("00:1b:dc:f2:2c:33");
+			//signal.waitSig();
+			signal.sendSig(1);
 			signal.sendSig(luggageList.get(0).getLuggageID());
+			//signal.sendSig(1);
+			signal.sendSig(LuggageCondition.waitDelivering);
+			signal.sendSig(ship_time);
 			signal.closeSig();
 		}catch(Exception e){
 			System.out.println("Sorry, Don't report.");
@@ -70,30 +80,58 @@ public class RelayStation {
 	 * 手順
 	 * メソッドに対応する操作を行う
 	 */
-	public void receiveDeliveryResult() {
+	public void receiveDeliveryResult() throws Exception{
 		int id = 0;
 		Date time = null;
+		Luggage luggage = null;
 		
 		try{
-			signal.openSig("DeliveryRobot");
+			//signal.openSig("00:16:53:5D:D3:BC");
 			signal.waitSig();
+			LCD.clear();
+			LCD.drawString("pass", 0, 2);
+			Delay.msDelay(3000);
 			String result = (String)signal.getSig();
+			LCD.clear();
+			LCD.drawString("result:"+result, 0, 2);
+			Delay.msDelay(3000);
 			if(result.equals("finished")){
-				time = (Date)signal.getSig();
+				time = new Date((long)signal.getSig());
 				id = (int)signal.getSig();
+				LCD.clear();
+				LCD.drawString("time"+time, 0, 2);
+				Delay.msDelay(5000);
+				LCD.refresh();
+				//System.out.println(time + "," + id);
 			}
 			else if(result.equals("absence")){
-				Luggage luggage = (Luggage)signal.getSig();
+				luggage = (Luggage)signal.getSig();
+				LCD.clear();
+				LCD.drawString("luggage:"+luggage.getLuggageID(), 0, 2);
+				Delay.msDelay(10000);
+				LCD.refresh();
 				luggageList.add(luggage);
+				LCD.clear();
+				LCD.drawString("ID:"+luggageList.get(0).getLuggageID(), 0, 2);
+				Delay.msDelay(10000);
+				LCD.refresh();
+				//System.out.println(luggageList.get(0).getLuggageID());
 			}
 			else if(result.equals("wrongAddress")){
-				Luggage luggage = (Luggage)signal.getSig();
+				luggage = (Luggage)signal.getSig();
 				wrongLugList.add(luggage);
+				LCD.clear();
+				LCD.drawString("ID:"+wrongLugList.get(0).getLuggageID(), 0, 2);
+				Delay.msDelay(2000);
+				LCD.refresh();
+				//System.out.println(wrongLugList.get(0).getLuggageID());
 			}
 			signal.closeSig();
-			this.reportDeliveryResult(result, id, time);
+			//this.reportDeliveryResult(result, id, time);
 		}catch(Exception e){
+			
 			System.out.println("Sorry, Don't receive report.");
+			throw e;
 		}
 	}
 
@@ -105,7 +143,6 @@ public class RelayStation {
 	 */
 	public void sendLugtoDeliveryRobot() {
 		try{
-			signal.openSig("DeliveryRobot");
 			signal.waitSig();
 			signal.getSig();
 			if(luggageList.isEmpty()){
@@ -117,10 +154,10 @@ public class RelayStation {
 				int id = luggage.getLuggageID();
 				DeliveryRecord dr = new DeliveryRecord(id, luggageList.get(0));
 				Date start = dr.getStartTime();
-				signal.sendSig(luggageList.isEmpty());
+				//signal.sendSig(luggageList.isEmpty());
 				signal.sendSig(luggage);
 				signal.closeSig();
-				this.reportDeliveryStart(id, start);
+				//this.reportDeliveryStart(id, start);
 			}
 		}catch(Exception e){
 			System.out.println("Sorry, Don't report.");
@@ -135,7 +172,7 @@ public class RelayStation {
 	 */
 	public void reportDeliveryResult(String result, int lug_id, Date fin_time) {
 		try{
-			signal.openSig("00:1b:dc:f2:2c:2c");
+			signal.openSig("00:1b:dc:f2:2c:33");
 			//signal.openSig("Headquarters");
 			//signal.waitSig();
 			if(result.equals("finished")){
@@ -144,8 +181,17 @@ public class RelayStation {
 			  signal.sendSig(LuggageCondition.finished);
 			  signal.sendSig(fin_time);
 			}
-			else{
-		  	  signal.sendSig(lug_id);
+			else if(result.equals("absence")){
+			  signal.sendSig(1);	
+			  signal.sendSig(lug_id);
+			  signal.sendSig(LuggageCondition.absence);
+			  signal.sendSig(fin_time);
+			}
+			else if(result.equals("wrongAddress")){
+			  signal.sendSig(1);	
+			  signal.sendSig(lug_id);
+		      signal.sendSig(LuggageCondition.wrongAddress);
+		      signal.sendSig(fin_time);
 			}
 			signal.closeSig();
 		}catch(Exception e){
@@ -163,14 +209,19 @@ public class RelayStation {
 	 */
 	public void reportDeliveryStart(int id, Date start) {
 		try{
-			signal.openSig("Headquarters");
-			signal.waitSig();
+			//signal.openSig("Headquarters");
+			signal.openSig("00:1b:dc:f2:2c:2c");
+			//signal.waitSig();
 			signal.sendSig(id);
 			signal.sendSig(start);
 			signal.closeSig();
 		}catch(Exception e){
 			System.out.println("Sorry, Don't report.");
 		}
+	}
+	
+	public ArrayList<Luggage> getLuggageList(){
+		return luggageList;
 	}
 
 }
