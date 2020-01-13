012c8ef49e7ae9ba5edb21490a629993a0149ed1
keita-kaneko <58538363+keita-kaneko@users.noreply.github.com>
2020-01-13 13:59:57 +0900

    Add files via upload

--- receiveSystem/Receiver.java
+++ receiveSystem/Receiver.java
@@ -1,73 +1,43 @@
 package receiveSystem;
 import java.io.IOException;
-import signal.*;
+import signal.PCSignal;
 public class Receiver {
-/**
- * 受取人名
- */
-	private static String receiverName;
-	/**
-	 * 受取人住所
-	 */
-	private static Integer receiverAddress;
+	
+	  private static String receiverName;
+	  private static Integer receiverAddress;
 
-
-	Receiver(String name,Integer ad){
-
+	  public Receiver(String name,Integer ad){
 		receiverName = name;
-
 		receiverAddress = ad;
+	  }
 
-	}
-
-	public static void main(String[] args) {
-		new Receiver("1",1);//ここで情報を変える
-
-		while(true){
-			getLug();
+	  /*public static void main(String[] args) {
+		  Receiver r = new Receiver("saito",3);//ここで情報を変える
+	      try{
+	    	while(true){
+	    		r.getLug();
+	    	}
+	      }catch(IOException e){
+	    	e.printStackTrace();
+	      }
+	  }*/
+	       
+	  public void getLug() throws IOException{
+	    PCSignal sig1 = new PCSignal();
+		sig1.waitSig();
+		sig1.getSig();
+		sig1.sendSig("Exist");
+		if(sig1.getSig().equals("false")) sig1.closeSig();    //false→10秒以内に返信できなかった
+		else{
+		    sig1.sendSig(receiverName);
+		    sig1.sendSig(receiverAddress);
+		    if(sig1.getSig().equals("false")) sig1.closeSig();
+		    else{
+		    	sig1.getSig();     //荷物受け取り
+		    	System.out.println("received.");
+		    	sig1.closeSig();
+		    }
 		}
-		
-	}
-
-
-	/**
-	 * 配達担当ロボットから荷物を受け取る
-	 */
-	private static void getLug(){
-		try{
-			PCSignal sig1 = new PCSignal();
-
-			sig1.waitSig();
-
-			sig1.getSig();
-
-			sig1.sendSig("Exist");
-
-			if(sig1.getSig().equals("false")) sig1.closeSig();    //false→10秒以内に返信できなかった
-
-			else{
-
-				sig1.sendSig(receiverName);
-
-				sig1.sendSig(receiverAddress);
-
-
-				if(sig1.getSig().equals("false")) sig1.closeSig();
-
-				else{
-
-					sig1.getSig();     //荷物受け取り
-
-					sig1.closeSig();
-
-				}
-
-			}
-
-		}catch(Exception e){
-			System.out.println("error.");
-		}  
-		
-	}
+	  }
 
 }
