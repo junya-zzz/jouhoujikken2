15b61635ea2879b8c77648472cf9541cfb91ffbe
koki17037 <58761220+koki17037@users.noreply.github.com>
2020-01-20 20:12:24 +0900

    Add files via upload

--- headquartersSystem/Headquarters.java
+++ headquartersSystem/Headquarters.java
@@ -1,6 +1,7 @@
 package headquartersSystem;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.Date;
 
 import javax.swing.plaf.basic.BasicSliderUI.TrackListener;
@@ -9,7 +10,9 @@ import java.util.Calendar;
 
 import recordSystem.DeliveryRecord;
 import recordSystem.DeliveryRecordList;
+import recordSystem.Luggage;
 import recordSystem.LuggageCondition;
+import recordSystem.RequestInformation;
 import signal.EV3Signal;
 import signal.PCSignal;
 import signal.Port;
@@ -17,10 +20,13 @@ import signal.Port;
 public class Headquarters{
 	public static final int ADD_RECORD = 0;
 	public static final int UPDATE_RECORD = 1;
-
+	public static final int CLUC_RECEIPTTIME = 2;
 	public static final int TRACK_LUGGAGE = 3;
+	public static final int UPDATE_FIXEDLUGLIST = 4;
+	public static final int SEND_FIXEDLUG = 5;
 
 	private DeliveryRecordList DeliveryRecordList = new DeliveryRecordList();
+	private ArrayDeque<Luggage> fixedLugList = new ArrayDeque<>();
 
 	public static void main(String[] args) {
 		Headquarters h = new Headquarters();
@@ -55,6 +61,20 @@ public class Headquarters{
 					
 				} else if (methodFlag == TRACK_LUGGAGE) {
 					sendLuggageInfomation(sig);
+					
+					
+				}else if (methodFlag == UPDATE_FIXEDLUGLIST){//宛先間違い修正　受付所-本部
+					Luggage fixedLug = (Luggage)sig.getSig();
+					fixedLugList.add(fixedLug);
+					DeliveryRecordList.fixDeliveryRecord(fixedLug.getLuggageID(),fixedLug);
+					
+				}else if(methodFlag == SEND_FIXEDLUG){//宛先間違い修正　中継所-本部
+					if(fixedLugList.isEmpty())sig.sendSig(0);
+					else sig.sendSig(fixedLugList.size());
+					
+					for(int i = 0;i<fixedLugList.size();i++){
+						sig.sendSig(fixedLugList.remove());
+					}
 				}
 				System.out.println(getDeliveryRecordList());
 				sig.closeSig();
